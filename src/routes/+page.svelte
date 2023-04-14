f9a5a621b5076a4859c53e65b77f066144f9f42b
Matúš Ferčák <74977647+MatusFercak@users.noreply.github.com>
2023-04-15 00:07:59 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,108 @@
+<main class="main-content">
+	<div class="top-cover u-padding-block-end-56 astro-DCNASTZX">
+		<div class="container astro-DCNASTZX">
+			<div class="u-flex u-gap-16 u-flex-justify-center u-margin-block-start-16 astro-DCNASTZX">
+				<h1 class="heading-level-3 astro-DCNASTZX">Appwrite Loves SSR</h1>
+				<code class="u-un-break-text astro-DCNASTZX" />
+			</div>
+			<p
+				class="body-text-1 u-normal u-margin-block-start-8 astro-DCNASTZX"
+				style="max-width: 56rem;"
+			>
+				This is a demo application. You can see authorized requests working both on client side, and
+				during pre-render. The whole process uses secure cookies, so this will only work if the
+				appwrite server is a subdomain of your frontend app.
+			</p>
+		</div>
+	</div>
+	<div class="container u-margin-block-start-negative-56">
+		<ul
+			class="grid-box"
+			style="--grid-gap:2rem; --grid-item-size:24rem; --grid-item-size-small-screens:16rem;"
+		>
+			<li>
+				<div class="card">
+					<div class="grid-item-1">
+						<div class="grid-item-1-start-start">
+							<div class="eyebrow-heading-3">Server Side</div>
+							<h2 class="heading-level-6 u-margin-block-start-8">Signed.</h2>
+						</div>
+					</div>
+					<div class="grid-item-1-end-end">
+						<svg
+							style="color: hsl(var(--color-text-warning));"
+							width="10"
+							height="10"
+							viewBox="0 0 8 8"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<circle cx="4" cy="4" r="4" fill="currentColor" />
+						</svg>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="card">
+					<div class="grid-item-1">
+						<div class="grid-item-1-start-start">
+							<div class="eyebrow-heading-3">Client Side</div>
+							<h2 class="heading-level-6 u-margin-block-start-8">Signed.</h2>
+						</div>
+						<div class="grid-item-1-start-end">
+							<div class="status">
+								<button class="tag">
+									<span class="text">Fetch</span>
+								</button>
+							</div>
+						</div>
+					</div>
+					<div class="grid-item-1-end-end">
+						<svg
+							style="color: hsl(var(--color-text-danger));"
+							width="10"
+							height="10"
+							viewBox="0 0 8 8"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<circle cx="4" cy="4" r="4" fill="currentColor" />
+						</svg>
+					</div>
+				</div>
+			</li>
+		</ul>
+	</div>
+	<!-- <div class="container">
+		<div class="card">
+			<h2 id="states-colors" class="heading-level-7 pink-heading-2 astro-XFJI5RRG">
+				<a href="#states-colors" class="astro-XFJI5RRG"> States Colors </a>
+			</h2>
+		</div>
+	</div> -->
+
+	<div class="container">
+		<article
+			class="card u-grid u-cross-center u-width-full-line common-section"
+			style="
+    background: linear-gradient(180deg, hsl(var(--p-card-bg-color)) 0%, hsl(var(--p-card-bg-color)) 12%, hsl(var(--p-body-bg-color)) 100%);
+    border: none;
+"
+		>
+			<div
+				class="u-flex u-flex-vertical u-cross-center u-gap-32 u-margin-block-start-40 u-padding-block-end-56"
+			>
+				<div class="u-text-center">
+					<h2 class="heading-level-3 u-trim-1">Manage Authorization</h2>
+					<p class="body-text-2 u-bold u-margin-block-start-8">
+						Create an account to make authorization requests.
+					</p>
+				</div>
+				<div class="u-flex u-gap-16 u-main-center">
+					<button class="button" type="button">Create Anonymous Account</button>
+				</div>
+			</div>
+		</article>
+		<div class="u-margin-block-end-50" />
+	</div>
+</main>
